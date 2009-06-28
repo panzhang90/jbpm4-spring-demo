f5a1a126add62a7c9700a1e9ddf533eed0c5473b
andries.inze <andries.inze@37df6b5c-4245-11de-998b-8fedbbe774c7>
2009-06-28 18:12:42 +0000

    timer test

--- trunk/src/test/java/be/inze/spring/demo/process/RegistrationProcessTests.java
+++ trunk/src/test/java/be/inze/spring/demo/process/RegistrationProcessTests.java
@@ -1,7 +1,11 @@
 package be.inze.spring.demo.process;
 
+import java.util.Collection;
+
+import org.jbpm.api.Execution;
 import org.jbpm.api.ProcessInstance;
 import org.jbpm.api.activity.ActivityExecution;
+import org.jbpm.api.job.Job;
 import org.jbpm.spring.test.AbstractTransactionalSpringJbpmTestCase;
 
 public class RegistrationProcessTests extends
@@ -35,9 +39,28 @@ public class RegistrationProcessTests extends
 	public void testSignalAccept() {
 		testStart();
 		
-		ProcessInstance processInstance = executionService.signalExecutionById(processInstanceId, "transition");
+		Execution execution = executionService.findExecutionById(processInstanceId);
+		
+		ProcessInstance processInstance = executionService.signalExecutionById(execution.getExecutions().iterator().next().getId(), "transition");
 		ActivityExecution activityExecution = (ActivityExecution) processInstance;
 		assertEquals("grant access", activityExecution.getActivityName());
+	}
+	
+	public void testTimeOut() {
+	  testStart();
+	  
+	  Collection<Job> result = managementService.createJobQuery().processInstanceId(processInstanceId).list();
+	  assertEquals(1, result.size());
+	  
+	  managementService.executeJob(result.iterator().next().getId());
+	  
+	  result = managementService.createJobQuery().processInstanceId(processInstanceId).list();
+      assertEquals(1, result.size());
+      
+      ProcessInstance instance = executionService.findProcessInstanceById(processInstanceId);
+      ActivityExecution activityExecution = (ActivityExecution) instance;
+      assertEquals("timed out", activityExecution.getActivityName());
+	  
 	}
 
 }
